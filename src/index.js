638f42ad8c1f1257eea954735ec8ef4a48c0d199
Samuel776 <hbnzsml@gmail.com>
2020-11-16 21:09:07 +0200

    feat:for testing
    this serves for testing the app
    this will allow the owner to test the app
    
    delivers

--- src/index.js
+++ src/index.js
@@ -1,42 +1,42 @@
-import '@babel/polyfill'
-import express from 'express';
-import mongoose from 'mongoose'
-import dotenv from 'dotenv';
-import postsRoute from './routes/posts.js';
-import authRoute from './routes/auth.js';
-import cors from 'cors';
-
+import "@babel/polyfill";
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import postsRoute from "./routes/posts.js";
+import authRoute from "./routes/auth.js";
+import cors from "cors";
 
 dotenv.config();
 
 // Routes middlewares
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-app.use('/posts', postsRoute);
-app.use('/api/user',authRoute);
+app.use("/posts", postsRoute);
+app.use("/api/user", authRoute);
 
-
-// Routes 
-app.get('/',(req,res)=>{
-    res.json({msg:'we are on home'});
+// Routes
+app.get("/", (req, res) => {
+  res.json({ msg: "we are on home" });
 });
 
 // connect to DB
-mongoose.connect(
-    process.env.NODE_ENV==="test"?
-      process.env.DB_CONNECTION_TEST:
-    process.env.DB_CONNECTION,
-   { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false } ,
-).then(()=>{
-    console.log('Connected to database')
-}).catch((error)=>{
-    console.log('Not connected')
-})
+mongoose
+  .connect(
+    process.env.NODE_ENV === "test"
+      ? process.env.DB_CONNECTION_TEST
+      : process.env.DB_CONNECTION,
+    { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
+  )
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((error) => {
+    console.log("Not connected");
+  });
 
 // start listening to the server
- const port = process.env.PORT || 3000
-app.listen(port, ()=>console.log('server started successfully'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log("server started successfully"));
 export default app;
-
